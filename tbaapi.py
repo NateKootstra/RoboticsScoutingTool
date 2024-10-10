75e4fb7d8a1f6b0e27687cd0eef36ae1c95f8662
Nate Kootstra <natekootstra@gmail.com>
2024-10-10 12:38:28 -0400

    Add comments.

--- tbaapi.py
+++ tbaapi.py
@@ -1,8 +1,11 @@
 import requests
 
-apiurl = "https://www.thebluealliance.com/api/v3"
+
+# NOTE: You need a TheBlueAlliance Read API v3 key to use this. Set the below variable to it was a string, or create a file to store it in.
 apikey = open("tbakey.txt").read()
 
+# Set up the basic API info.
+apiurl = "https://www.thebluealliance.com/api/v3"
 headers = {
     "Content-Type" : "application/json",
     "X-TBA-Auth-Key" : apikey
@@ -10,11 +13,12 @@ headers = {
 
 
 
-
+# General request function.
 def request(endpoint):
     return requests.get(url=apiurl+"/"+endpoint, headers=headers)
 
 
+# Class to retrieve API data for a specific year.
 class Year(object):
     def __init__(self, year):
         if not type(year) == int:
@@ -29,6 +33,7 @@ class Year(object):
         return request(f"events/{self.year}").json()
 
 
+# Class to retrieve API data for a specific district.
 class District(object):
     def __init__(self, key):
         if not type(key) == str:
@@ -49,6 +54,7 @@ class District(object):
         return request(f"district/{self.key}/teams").json()
 
 
+# Class to retrieve API data for a specific event.
 class Event(object):
     def __init__(self, key, name):
         if not type(key) == str:
@@ -67,6 +73,7 @@ class Event(object):
         return request(f"event/{self.key}/alliances").json()
 
 
+# Class to retrieve API data for a specific team.
 class Team(object):
     def __init__(self, team_number):
         if not type(team_number) == int:
